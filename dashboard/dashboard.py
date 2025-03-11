c494969227202c1c50e66dcbf670868f51f8c45c
hara29 <cindymaharani29@gmail.com>
2025-03-11 23:31:26 +0700

    menambahkan keterangan pada streamlit app

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -93,22 +93,30 @@ with col3:
 fig, ax = plt.subplots(figsize=(12, 6))
 ax.plot(monthly_data['Bulan'], monthly_data['Total peminjaman'], marker='o', color=set2_palette[1])
 ax.set_xlabel("Bulan", fontsize=12)
-ax.set_ylabel("Jumlah Penyewa", fontsize=12)
+ax.set_ylabel("Jumlah Peminjam", fontsize=12)
 ax.tick_params(axis='x', rotation=45, labelsize=10)
 ax.grid(axis='x', linestyle='--', alpha=0.5)
 st.pyplot(fig)
 
+with st.expander("Penjelasan"):
+    st.write("""
+    - Peminjaman sepeda pada pertengahan hingga akhir 2012 lebih tinggi dibandingkan tahun sebelumnya, menandakan **adanya peningkatan tren penggunaan sepeda secara keseluruhan (2 tahun terakhir)**.
+    - **Terdapat penurunan jumlah peminjaman yang drastis menjelang akhir tahun baik pada tahun 2011 maupun 2012.** Pada tahun 2011 terjadi penurunan jumlah peminjam pada bulan juli sampai puncaknya pada bulan november sebesar 	17.28%. Pada tahun 2012, penurunan jumlah peminjam terjadi mulai bulan oktober sebesar 9% dan semakin berkurang pada bulan november sebesar 23.22%. Hal ini kemungkinan **disebabkan oleh faktor musim atau cuaca** (misalnya, musim dingin yang mengurangi aktivitas luar ruangan).
+    """)
+
 # Menampilkan visualisasi peminjaman antar musim
-st.subheader("Perbandingan Total Penyewa Antar Musim")
+st.subheader("Perbandingan Total Peminjam Antar Musim")
 fig, ax = plt.subplots(figsize=(8, 5))
 max_value = seasonal_data['count_rental'].max()
 colors = [set2_palette[1] if val == max_value else set2_palette[2] for val in seasonal_data['count_rental']]
 sns.barplot(x='season', y='count_rental', data=seasonal_data, palette=colors, ax=ax)
 ax.set_xlabel("Musim", fontsize=12)
-ax.set_ylabel("Total Penyewa", fontsize=12)
+ax.set_ylabel("Total Peminjam", fontsize=12)
 ax.set_title("Total peminjaman Sepeda Berdasarkan Musim", fontsize=14)
 ax.yaxis.set_major_formatter(FuncFormatter(lambda x, _: f"{int(x):,}"))
 st.pyplot(fig)
+with st.expander("Penjelasan"):
+    st.write('Musim Gugur (Fall) memiliki jumlah peminjaman tertinggi sedangkan Musim Semi (Spring) memiliki jumlah peminjam terendah pada tahun 2011, tetapi meningkat cukup signifikan pada tahun 2012.')
 
 # Menampilkan visualisasi hubungan suhu, kelembapan, dan kecebapan angin terhadap peminjaman
 st.subheader("Pengaruh Kondisi Lingkungan Terhadap peminjaman")
@@ -119,9 +127,9 @@ sns.regplot(x="humidity", y="count_rental", data=main_df_day, scatter_kws={'alph
 sns.regplot(x="windspeed", y="count_rental", data=main_df_day, scatter_kws={'alpha':0.5}, ax=axes[2], color=set2_palette[2])
 st.pyplot(fig)
 with st.expander("Penjelasan Korelasi"):
-    st.write("Suhu memiliki korelasi positif terhadap jumlah penyewa. Kelembapan dan kecepatan angin memiliki korelasi negatif yang lemah terhadap jumlah penyewa.")
+    st.write("Suhu memiliki korelasi positif terhadap jumlah peminjam. Kelembapan dan kecepatan angin memiliki korelasi negatif yang lemah terhadap jumlah peminjam.")
 
-# Menampilkan visualisasi penyewa terdaftar vs kasual
+# Menampilkan visualisasi peminjam terdaftar vs kasual
 st.subheader("Pola Pengguna Terdaftar dan Pengguna Kasual")
 tabs = st.tabs(["Pola peminjaman Sepeda per Hari", "Pola peminjaman Sepeda per Jam"])
 
@@ -131,7 +139,7 @@ with tabs[0]:
     ax.plot(weekly_rental_data['weekday'], weekly_rental_data['casual'], marker='o', linestyle='-', color=set2_palette[1], label='Casual')
     ax.plot(weekly_rental_data['weekday'], weekly_rental_data['registered'], marker='o', linestyle='-', color=set2_palette[2], label='Registered')
     ax.set_xlabel("Hari dalam Seminggu", fontsize=12)
-    ax.set_ylabel("Jumlah Penyewa", fontsize=12)
+    ax.set_ylabel("Jumlah Peminjam", fontsize=12)
     ax.grid(axis='y', linestyle='--', alpha=0.7)
     ax.legend()
     st.pyplot(fig)
@@ -146,7 +154,14 @@ with tabs[1]:
     ax.legend()
     st.pyplot(fig)
 
-# Menampilkan visualisasi peminjaman antar musim
+with st.expander('Penjelasan'):
+    st.write(""" 
+  - **Pengguna terdaftar (registered) lebih suka meminjam sepeda pada hari kerja (senin - jumat)** sedangkan **pengguna biasa (casual) lebih senang meminjam sepeda pada akhir pekan (sabtu - minggu)**.
+  - Pengguna terdaftar memiliki pola peminjaman dengan dua puncak utama, yaitu pagi hari (08:00) dan Sore hari (17:00 - 18:00). Pengguna kasual memiliki pola yang lebih stabil sepanjang hari, dengan peminjaman meningkat secara bertahap hingga siang/malam.
+  - Ini menunjukkan bahwa **pengguna terdaftar lebih sering menggunakan sepeda untuk keperluan komuter** (perjalanan kerja/sekolah), sementara **pengguna kasual lebih sering menyewa untuk rekreasi**.
+    """)
+
+# Menampilkan visualisasi segmentasi peminjaman sepeda denga Bining
 st.subheader("Segmentasi Jumlah peminjaman Sepeda dengan Binning")
 cluster_colors = {'Low': '#1f77b4', 'Medium': '#ff7f0e', 'High': '#d62728'}
 fig, ax = plt.subplots(figsize=(12, 5))
@@ -155,5 +170,11 @@ ax.set_xlabel("Jam dalam Sehari")
 ax.set_ylabel("Jumlah peminjaman")
 ax.legend(title="Cluster")
 st.pyplot(fig)
+with st.expander('Penjelasan:'):
+    st.write("""
+    - **Jam dengan peminjaman Tinggi (High - Merah)**: Terjadi lonjakan signifikan pada jam 8 pagi dan 17-18 sore, yang kemungkinan besar terkait dengan jam kerja (morning & evening rush hours). Banyak orang meminjam sepeda untuk pergi dan pulang kerja/sekolah. Oleh karena itu, **stok sepeda pada jam - jam ini dapat diperbanyak untuk mengantisipasi lonjakan peminjaman**.
+    - **Jam dengan peminjaman Sedang (Medium - Oranye)**: peminjaman sepeda berada di tingkat menengah pada jam 6-7 pagi, 9-11 siang, serta 14-16 sore dan 20-23 malam. Ini bisa mencerminkan waktu luang, istirahat makan siang, atau aktivitas rekreasi.
+    - **Jam dengan peminjaman Rendah (Low - Biru)**: peminjaman cenderung rendah antara tengah malam hingga subuh (0-5 pagi) dan beberapa jam di siang hari. Aktivitas peminjaman sepeda minim di waktu-waktu ini karena mayoritas orang sedang tidur.
+    """)
 
 st.caption('Copyright (c) Cindy@LaskarAi 2024')
